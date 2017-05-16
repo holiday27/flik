cb61e2c29ebfd919b04dc4fe2369bc89d97b1568
Unknown <igor-matsuzaki@live.com>
2017-05-16 13:08:57 -0300

    Merge Schema
    Merging schema Entity Message at schema.js

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,100 @@
 const {
   GraphQLObjectType,
   GraphQLList,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString
 } = require('graphql');
 
 const entity = require('../models/Entity');
+const reply = require('../models/Reply');
 const messages = require('../models/Message');
-const messageType = require('./messageType');
-const entityType = require('./entityType');
+const replyType = require('./replyType');
+// const messageType = require('./messageType');
+// const entityType = require('./entityType');
+
+const entityType = new GraphQLObjectType({
+  name: 'entity',
+  description: 'Entity',
+  fields: () => ({
+    id: {
+      type: GraphQLInt,
+      resolve(tableEntity) {
+        return tableEntity.get('id');
+      }
+    },
+    name: {
+      type: GraphQLString,
+      resolve(tableEntity) {
+        return tableEntity.get('name');
+      }
+    },
+    created: {
+      type: GraphQLString,
+      resolve(tableEntity) {
+        return tableEntity.get('created');
+      }
+    },
+    modified: {
+      type: GraphQLString,
+      resolve(tableEntity) {
+        return tableEntity.get('modified');
+      }
+    },
+    messages: {
+      type: new GraphQLList(messageType)
+    }
+  })
+});
+
+
+const messageType = new GraphQLObjectType({
+  name: 'messages',
+  description: 'Messages',
+  fields: () => ({
+    id: {
+      type: GraphQLInt,
+      resolve(tableMessage) {
+        return tableMessage.get('id');
+      }
+    },
+    content: {
+      type: GraphQLString,
+      resolve(tableMessage) {
+        return tableMessage.get('content');
+      }
+    },
+    created: {
+      type: GraphQLString,
+      resolve(tableMessage) {
+        return tableMessage.get('created');
+      }
+    },
+    modified: {
+      type: GraphQLString,
+      resolve(tableMessage) {
+        return tableMessage.get('modified');
+      }
+    },
+    replies: {
+      type: new GraphQLList(replyType)
+
+
+      // resolve(tableMessage) {
+      //   // console.log('tableType', tableMessage.toJSON());
+      //   messages.forge({ id: tableMessage.get('id') }).fetch({ withRelated: 'replies' }).then((message) => {
+      //     console.log(tableMessage.get('id'));
+      //     console.log(replies);
+      //     return replies;
+      //   });
+      // }
+    },
+    entities: {// this.belongsToMany('entity_message', {
+      type: new GraphQLList(entityType),
+      description: 'Represents entitites on the message'
+    }// )
+  })
+});
 
 
 const Query = new GraphQLObjectType({
